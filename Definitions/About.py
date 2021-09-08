d11f74deddba6806d76790816e51e0baac043f71
Slavek592 <71459428+Slavek592@users.noreply.github.com>
2021-09-08 18:38:19 +0200

    New feautures

--- Definitions/About.py
+++ Definitions/About.py
@@ -9,6 +9,7 @@ def about():
     info.grid(row=0, column=0)
     Label(info, text="Authors:").grid(row=0, column=0)
     Label(info, text="Viacheslav Nikiforov").grid(row=1, column=0)
+    Label(info, text="Zbyněk Vávra").grid(row=2, column=0)
     Label(info, text="Time of work: 2021").grid(row=0, column=1)
     Button(about_root, text="Close", command=lambda: about_root.destroy()).grid(row=1, column=0)
     about_root.mainloop()
--- Definitions/Planet.py
+++ Definitions/Planet.py
@@ -5,13 +5,34 @@ from Definitions import About
 
 def planet():
     def new_turn():
+        new_money = money_label["text"] - population_label["text"] + industry_label["text"]
+        new_pollution = pollution_label["text"] + population_label["text"] + industry_label["text"]
+        new_carbon = carbon_label["text"] - round(nature_label["text"] / 4) + industry_label["text"]
+        new_nature = nature_label["text"] - round(industry_label["text"] / 4)
         turn_label.configure(text=turn_label["text"] + 1)
-        money_label.configure(text=money_label["text"] + population_label["text"])
-        pollution_label.configure(text=pollution_label["text"] + 1)
+        money_label.configure(text=new_money)
+        pollution_label.configure(text=new_pollution)
+        carbon_label.configure(text=new_carbon)
+        nature_label.configure(text=new_nature)
 
     def cleaning_project():
         money_label.configure(text=money_label["text"] - 5)
         pollution_label.configure(text=pollution_label["text"] - 10)
+
+    def burning_project():
+        money_label.configure(text=money_label["text"] + 5)
+        pollution_label.configure(text=pollution_label["text"] - 10)
+        carbon_label.configure(text=carbon_label["text"] + 10)
+
+    def planting_project():
+        money_label.configure(text=money_label["text"] - 10)
+        nature_label.configure(text=nature_label["text"] + 5)
+        carbon_label.configure(text=carbon_label["text"] - 5)
+
+    def building_project():
+        industry_label.configure(text=industry_label["text"] + 1)
+        nature_label.configure(text=nature_label["text"] - 5)
+        carbon_label.configure(text=carbon_label["text"] + 5)
 
     def exit_game():
         print("Thanks for using!")
@@ -45,10 +66,29 @@ def planet():
     Label(right, text="Pollution: ").grid(row=2, column=0)
     pollution_label = Label(right, text=0)
     pollution_label.grid(row=2, column=1)
+    Label(right, text="Industry: ").grid(row=3, column=0)
+    industry_label = Label(right, text=12)
+    industry_label.grid(row=3, column=1)
+    Label(right, text="Nature: ").grid(row=4, column=0)
+    nature_label = Label(right, text=50)
+    nature_label.grid(row=4, column=1)
+    Label(right, text="Carbon: ").grid(row=5, column=0)
+    carbon_label = Label(right, text=50)
+    carbon_label.grid(row=5, column=1)
 
     projects = Frame(root)
     projects.grid(row=0, column=3)
-    cleaning_button = Button(projects, text="Cleaning\nMoney - 5\nPollution - 10", command=lambda: cleaning_project())
+    cleaning_button = Button(projects, text="Cleaning\nMoney - 5\nPollution - 10",
+                             command=lambda: cleaning_project())
     cleaning_button.grid(row=0, column=0)
+    burning_button = Button(projects, text="Burning\nMoney + 5\nPollution - 10\nCarbon + 10",
+                            command=lambda: burning_project())
+    burning_button.grid(row=0, column=1)
+    planting_button = Button(projects, text="Planting\nMoney - 10\nNature + 5\nCarbon - 5",
+                             command=lambda: planting_project())
+    planting_button.grid(row=1, column=0)
+    building_button = Button(projects, text="Building\nIndustry + 1\nNature - 5\nCarbon + 5",
+                             command=lambda: building_project())
+    building_button.grid(row=1, column=1)
 
     root.mainloop()
